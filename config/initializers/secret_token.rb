ecff961f246498d6ab7b15cbc4d6218a92f55d15
Evan Chen <ebzlo@ebzlo.com>
2012-11-01 00:11:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiles::Application.config.secret_token = 'e2ff9343ef4e260e45480fb69bc3dc46e779d0a1a904a438397d6b37796757a30e408ef5d1f9ca3a0e133f669444b10ea0f85c790fea01cded2698aba6233780'
